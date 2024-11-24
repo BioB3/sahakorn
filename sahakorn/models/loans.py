db09bbe5a2893100cd0e749121e2a77ea4b57c18
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-24 21:35:32 +0700

    fix wromg nodels

--- sahakorn/models/loans.py
+++ sahakorn/models/loans.py
@@ -6,10 +6,20 @@ from .member import Member
 class Loans(models.Model):
     """Model to record loans taken by members"""
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(
+                    paid__lte=models.F("amount"),
+                ),
+                name="paid amount less than borrow amount",
+            )
+        ]
+
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
-    amount = models.FloatField()
+    amount = models.FloatField(validators=[MinValueValidator(0)])
     loan_date = models.DateField()
-    payed = models.FloatField(validators=[MinValueValidator(0)])
+    paid = models.FloatField(validators=[MinValueValidator(0)])
 
     def __str__(self):
         return f"Loan of {self.member} from {self.loan_date}"
--- sahakorn/models/market_item.py
+++ sahakorn/models/market_item.py
@@ -2,17 +2,25 @@ from django.db import models
 from django.core.validators import MinValueValidator
 from .stock import Stock
 from .item_type import ItemType
-from .member import Member
 
 
 class MarketItem(models.Model):
     """Model for Market items"""
 
-    p_name = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     quantity = models.IntegerField(validators=[MinValueValidator(0)])
-    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
-    producer = models.ForeignKey(Member, on_delete=models.CASCADE)
+    item_type = models.ManyToManyField(ItemType)
     price = models.IntegerField(validators=[MinValueValidator(0)])
 
+    @property
+    def p_name(self) -> str:
+        """Get product name."""
+        return self.stock.name
+
+    @property
+    def member(self) -> str:
+        """return the god damn member"""
+        return self.stock.member
+
     def __str__(self):
-        return f"{self.p_name} x {self.quantity} by {self.producer} "
+        return f"{self.p_name} x {self.quantity} by {self.member} "
