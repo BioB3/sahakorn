e3a6e3b0103134b7bf27b56e2fab46b6b15aeb62
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-24 20:03:54 +0700

    fixed many-to-many relation

--- sahakorn/models/stock.py
+++ sahakorn/models/stock.py
@@ -9,7 +9,7 @@ class Stock(models.Model):
     member = models.ForeignKey(Member, on_delete=models.CASCADE)
     p_name = models.CharField(max_length=300)
     quantity = models.IntegerField()
-    type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    item_type = models.ManyToManyField(ItemType)
 
     def __str__(self):
         return f"{self.quantity} {self.p_name} of {self.member}"
