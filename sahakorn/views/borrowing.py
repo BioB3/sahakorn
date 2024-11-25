41100baef8cafec18af02a6de80522111d45314e
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-25 21:15:56 +0700

    - for all of my equipment find which ones are currently borrowed - List all members that haven't payed the common fee

--- sahakorn/views/borrowing.py
+++ sahakorn/views/borrowing.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
 from sahakorn.models import Borrowing
@@ -14,3 +16,11 @@ class BorrowingViewSet(ModelViewSet):
 
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+
+    def list(self, request, *args, **kwargs):
+        member = request.query_params("member")
+        if member is not None:
+            queryset = Borrowing.objects.filter(user=member)
+            serializer = BorrowingSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return super().list(request, *args, **kwargs)
--- sahakorn/views/common_fee.py
+++ sahakorn/views/common_fee.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
 from sahakorn.models import CommonFee
@@ -14,3 +17,9 @@ class CommonFeeViewSet(ModelViewSet):
 
     queryset = CommonFee.objects.all()
     serializer_class = CommonFeeSerializer
+
+    @action(detail=False, methods=["post"])
+    def not_paid(self, request):
+        queryset = CommonFee.objects.filter(total_paid=0)
+        serializer = CommonFeeSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
