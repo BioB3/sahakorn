8c9258e3572775cb4f722470536b31f421283904
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-25 20:30:19 +0700

    basically filter transactions by ItemType since all of them are sell orders anyways or maybe filter by person

--- sahakorn/views/transaction.py
+++ sahakorn/views/transaction.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
 from sahakorn.models import Transaction
@@ -14,3 +16,28 @@ class TransactionViewSet(ModelViewSet):
 
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    def list(self, request, *args, **kwargs):
+        item_type = request.query_params.get("item_type")
+        member = request.query_params.get("member")
+        if item_type or member:
+            if item_type is not None and member is not None:
+                queryset = Transaction.objects.filter(
+                    market_item__stock__item_type=item_type,
+                    member=member,
+                )
+
+            elif item_type is not None:
+                queryset = Transaction.objects.filter(
+                    market_item__stock__item_type=item_type
+                )
+
+            elif member is not None:
+                queryset = Transaction.objects.filter(member=member)
+
+            return Response(
+                TransactionSerializer(queryset, many=True).data,
+                status=status.HTTP_200_OK,
+            )
+
+        return super().list(request, *args, **kwargs)
