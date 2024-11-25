f3a1cafeec47dfa80ca8ec123fd02b2243b3744d
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-25 20:21:53 +0700

    Filter market item by type

--- sahakorn/views/market_item.py
+++ sahakorn/views/market_item.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.serializers import ModelSerializer
 from sahakorn.models import MarketItem
@@ -14,3 +16,11 @@ class MarketItemViewSet(ModelViewSet):
 
     queryset = MarketItem.objects.all()
     serializer_class = MarketItemSerializer
+
+    def list(self, request, *args, **kwargs):
+        item_type = request.query_params.get("item_type")
+        if request.query_params.get("item_type"):
+            queryset = MarketItem.objects.filter(item_type=item_type)
+            serializer = MarketItemSerializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return super().list(request, *args, **kwargs)
