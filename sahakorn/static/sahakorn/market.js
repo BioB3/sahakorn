5f1091ae0b70458807b63ce2bd7e249b6c618028
Rattapoom Pethputong <celica2548@gmail.com>
2024-11-27 19:38:51 +0700

    did some market go eat dinner lah

--- sahakorn/static/sahakorn/market.js
+++ sahakorn/static/sahakorn/market.js
@@ -5,10 +5,21 @@ const filterBtn = {}
 // btnName: actualBtn
 
 async function populateMarketTable(marketItems) {
-  for (marketItem of marketItems) {
-    // append to table
-    // another for loop in here
-    // if there's a filter that's not here yet, make new btn
+  // not test yet.
+  for (let marketItem of marketItems) {
+    const stock = await universalFetcher(`stock/${marketItem.stock}`)
+    console.log(stock)
+    // const itemType = await universalFetcher(`item_type/${stock.item_type}`)
+    const row = document.createElement('tr')
+    row.innerHTML = `
+      <td>${stock.p_name}</td>
+      <td>${stock.item_type}</td>
+      <td>${stock.member}</td>
+      <td>${marketItem.quantity}</td>
+      <td>${marketItem.price}</td>
+      <td>Never gonna run around and desert you</td>
+    `
+    document.getElementById('market-table').appendChild(row)
   }
 }
 
@@ -17,5 +28,7 @@ function generateBtn(name) {
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
-
+  const marketItems = await universalFetcher('market_item')
+  console.log(marketItems)
+  populateMarketTable(marketItems)
 })
